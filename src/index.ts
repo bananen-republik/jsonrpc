35e5cf80b2942bafb1103fd81a6a71df5f05edc7
bdf2ch <kolu0897@nw.mrsksevzap.ru>
2017-04-04 11:33:02 +0300

    1.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { JsonRpcModule } from './json-rpc.module';
+export { JsonRpcClientService } from './json-rpc-client.service';
 export { JsonRpcResponse, JsonRpcResponseInterface } from './json-rpc-response.model';
 export { JsonRpcRequest, JsonRpcRequestInterface } from './json-rpc-request.model';
 export { JsonRpcError, JsonRpcErrorInterface } from './json-rpc-error.model';
